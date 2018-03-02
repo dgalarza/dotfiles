a0e2115a1066ffa52611cdb97dc499c0e1982285
Damian Galarza <damian@thoughtbot.com>
2018-03-02 14:22:04 -0500

    Support multiple polybars for monitors
    * Configure separate polybar for the external display
    * Remove unused modules
    * Remove unused configuration

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,8 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar dgalarza &
-
+if type "xrandr"; then
+  for monitor in $(xrandr -q | grep " connected" | cut -d " " -f1); do
+    polybar ${monitor}&
+  done
+fi
 
 echo "Bars launched..."
