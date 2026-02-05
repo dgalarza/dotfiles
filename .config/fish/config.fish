9d96ea561d79ef3437911ebb1b72921e6f96bc0a
Damian Galarza <damian@augusthealth.com>
2026-02-05 15:06:44 -0500

    Adjust fish config 1. Enable experimental TEAMS feature for Claude Code https://code.claude.com/docs/en/agent-teams#enable-agent-teams
    2. Alias ls as eza
    3. Enable LSP in claude code

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,12 @@
 fish_add_path $HOME/.local/bin
 
+set ENABLE_LSP_TOOLS 1
+set CLAUDE_CODE_EXPERIMENTAL_AGENT_TEAMS 1
+
 set -gx fish_function_path ~/.config/fish/functions_local $fish_function_path
 
 alias vim=nvim
+alias ls=eza
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
